1a906fe3ac395ef29c6612a936ad7d377d793d63
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-01-22 15:44:54 +1030

    added env file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from "./app"
+import { env } from "./env"
 
 app.listen({
-  port: 3333,
+  port: env.PORT,
   host: "0.0.0.0",
 }).then(() => {
   console.log("🚀 HTTP server running on port 3333")
