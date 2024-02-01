6e7c57c7d63320b373bb30ff4fef0b227d09956b
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-02-02 00:58:29 +1030

    separete functions

--- src/use-cases/create-budget.spec.ts
+++ src/use-cases/create-budget.spec.ts
@@ -1,26 +1,22 @@
 import { InMemoryBudgetsRepository } from "@/repositories/In-memory/in-memory-budgets-repository";
 import { CreateBudgetUseCase } from "./create-budget";
-import { InMemoryUserBudgetRepository } from "@/repositories/In-memory/in-memory-userBudget-repository";
+
 import { beforeEach, describe, expect, it } from "vitest";
 
 let inMemoryBudgetsRepository: InMemoryBudgetsRepository;
-let inMemoryUserBudgetRepository: InMemoryUserBudgetRepository;
+
 let sut: CreateBudgetUseCase;
 
 describe("Create Budget Use Case", () => {
   beforeEach(() => {
     inMemoryBudgetsRepository = new InMemoryBudgetsRepository();
-    inMemoryUserBudgetRepository = new InMemoryUserBudgetRepository();
-    sut = new CreateBudgetUseCase(
-      inMemoryBudgetsRepository,
-      inMemoryUserBudgetRepository,
-    );
+
+    sut = new CreateBudgetUseCase(inMemoryBudgetsRepository);
   });
 
   it("should be able to create a new budget", async () => {
     const { budget } = await sut.execute({
       name: "New Budget",
-      user_id: "user-1",
     });
 
     expect(budget.id).toEqual(expect.any(String));
--- src/use-cases/create-budget.ts
+++ src/use-cases/create-budget.ts
@@ -1,10 +1,9 @@
 import { BudgetsRepository } from "@/repositories/budget-repository";
-import { UserBudgetsRepository } from "@/repositories/userBudget-repository";
+
 import { Budget } from "@prisma/client";
 
 interface CreateBudgetUseCaseRequest {
   name: string;
-  user_id: string;
 }
 
 interface CreateBudgetUseCaseResponse {
@@ -12,21 +11,12 @@ interface CreateBudgetUseCaseResponse {
 }
 
 export class CreateBudgetUseCase {
-  constructor(
-    private budgetsRepository: BudgetsRepository,
-    private userBudgetsRepository: UserBudgetsRepository,
-  ) {}
+  constructor(private budgetsRepository: BudgetsRepository) {}
 
   async execute({
     name,
-    user_id,
   }: CreateBudgetUseCaseRequest): Promise<CreateBudgetUseCaseResponse> {
     const budget = await this.budgetsRepository.create({ name });
-
-    await this.userBudgetsRepository.connectToUser({
-      user_id,
-      budget_id: budget.id,
-    });
 
     return { budget };
   }
