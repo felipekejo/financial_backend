46876b7af0ec8acf70891541c95b04f7e722146d
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-02-06 00:06:23 +1030

    in memory transactions

--- src/repositories/In-memory/in-memory-transactions-repository.ts
+++ src/repositories/In-memory/in-memory-transactions-repository.ts
@@ -9,12 +9,13 @@ export class InMemoryTransactionsRepository implements TransactionsRepository {
       id: data.id ?? randomUUID(),
       description: data.description,
       amount: data.amount,
-      type: data.type,
-      category_id: data.category_id,
-      subcategory_id: data.subcategory_id,
+      type: data.type ?? "EXPENSES",
+      category_id: data.category_id ?? null,
+      subcategory_id: data.subcategory_id ?? null,
       account_id: data.account_id,
-      created_at: data.created_at ?? new Date(),
+      created_at: new Date(),
     };
+
     this.items.push(transaction);
 
     return transaction;
@@ -35,27 +36,77 @@ export class InMemoryTransactionsRepository implements TransactionsRepository {
       (item) => item.account_id === accountId,
     );
 
-    if (!transactions) {
-      return null;
-    }
     return transactions;
   }
 
-  async findByCategoryId(categoryId: string, accountId: string) {}
+  async findByCategoryId(categoryId: string, accountId: string) {
+    const transactions = this.items.filter(
+      (item) =>
+        item.account_id === accountId && item.category_id === categoryId,
+    );
+    return transactions;
+  }
 
-  async findByType(type: "INCOMES" | "EXPENSES", accountId: string) {}
+  async findByType(type: "INCOMES" | "EXPENSES", accountId: string) {
+    const transactions = this.items.filter(
+      (item) => item.account_id === accountId && item.type === type,
+    );
+    return transactions;
+  }
 
-  async findByYear(year: string, accountId: string) {}
+  async findByYear(year: string, accountId: string) {
+    const transactions = this.items.filter(
+      (item) =>
+        item.account_id === accountId &&
+        new Date(item.created_at).getFullYear().toString() === year,
+    );
+    return transactions;
+  }
 
-  async findByYearAndMonth(year: string, month: string, accountId: string) {}
+  async findByYearAndMonth(year: string, month: string, accountId: string) {
+    const transactions = this.items.filter(
+      (item) =>
+        item.account_id === accountId &&
+        new Date(item.created_at).getFullYear().toString() === year &&
+        new Date(item.created_at).getMonth().toString() === month,
+    );
+    return transactions;
+  }
 
-  async findByDate(date: Date, accountId: string) {}
+  async findByDate(date: Date, accountId: string) {
+    const transactions = this.items.filter(
+      (item) =>
+        item.account_id === accountId &&
+        new Date(item.created_at).toDateString() === date.toDateString(),
+    );
+    return transactions;
+  }
 
-  async sumAmountByType(type: "INCOMES" | "EXPENSES", accountId: string) {}
+  async sumAmountByType(type: "INCOMES" | "EXPENSES", accountId: string) {
+    const sumAmount = this.items.reduce((acc, item) => {
+      if (item.account_id === accountId && item.type === type) {
+        acc += item.amount;
+      }
+      return acc;
+    }, 0);
+    return sumAmount;
+  }
 
   async sumAmountByYear(
     year: string,
     accountId: string,
     type: "INCOMES" | "EXPENSES",
-  ) {}
+  ) {
+    const sumAmount = this.items.reduce((acc, item) => {
+      if (
+        item.account_id === accountId &&
+        new Date(item.created_at).getFullYear().toString() === year &&
+        item.type === type
+      ) {
+        acc += item.amount;
+      }
+      return acc;
+    }, 0);
+    return sumAmount;
+  }
 }
