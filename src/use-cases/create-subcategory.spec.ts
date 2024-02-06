faa06af50c3620c4aa6ea87f50e2774f1080c33b
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-02-06 13:18:17 +1030

    finish create subcategory test

--- src/use-cases/create-subcategory.spec.ts
+++ src/use-cases/create-subcategory.spec.ts
@@ -1,13 +1,21 @@
 import { InMemorySubcategoriesRepository } from "@/repositories/In-memory/in-memory-subcategories-repository";
-import { beforeEach, describe } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { CreateSubcategoriesUseCase } from "./create-subcategory";
 
 let inMemorySubcategoriesRepository: InMemorySubcategoriesRepository;
 let sut: CreateSubcategoriesUseCase;
 
-describe("Should be to create a subcategory", () => {
+describe("Create Subcategory Use Case", () => {
   beforeEach(() => {
     inMemorySubcategoriesRepository = new InMemorySubcategoriesRepository();
     sut = new CreateSubcategoriesUseCase(inMemorySubcategoriesRepository);
+  });
+
+  it("should be able to create a new subcategory", async () => {
+    const { subcategory } = await sut.execute({
+      name: "new subcategory",
+      categoryId: "category-01",
+    });
+    expect(subcategory.id).toEqual(expect.any(String));
   });
 });
