4f95a6f2839d71afb8555cb0e3a8a7b49187e171
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-02-13 00:35:00 +1030

    e2e config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  // test: {
-  //   environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
-  //   dir: "src", // Essa linha
-  // },
+  test: {
+    environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
+    dir: "src", // Essa linha
+  },
 });
