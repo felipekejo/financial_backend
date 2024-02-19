95e8e2b61e73c64d8993d65ed1383cf1d3ef414a
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-02-20 08:53:30 +1030

    create transaction test

--- src/http/controllers/transactions/createTransaction.spec.ts
+++ src/http/controllers/transactions/createTransaction.spec.ts
@@ -1,4 +1,5 @@
 import { app } from "@/app";
+import { prisma } from "@/lib/prisma";
 import request from "supertest";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
@@ -11,14 +12,37 @@ describe("Create Transaction (e2e)", () => {
   });
 
   it("should be able to create a new transaction", async () => {
+    const budget = await prisma.budget.create({
+      data: {
+        name: "new budget",
+      },
+    });
+    const account = await prisma.account.create({
+      data: {
+        name: "new account",
+        budget_id: budget.id,
+      },
+    });
+    const category = await prisma.category.create({
+      data: {
+        name: "new category",
+      },
+    });
+    const subcategory = await prisma.subcategory.create({
+      data: {
+        name: "new subcategory",
+        category_id: category.id,
+      },
+    });
+
     const response = await request(app.server).post("/transactions").send({
       description: "new income",
       amount: 100,
       type: "INCOMES",
-      accountId: "account-01",
-      categoryId: "category-01",
-      subcategoryId: "subcategory-01",
+      accountId: account.id,
+      categoryId: category.id,
+      subcategoryId: subcategory.id,
     });
-    // expect(response.statusCode).toEqual(201);
+    expect(response.statusCode).toEqual(201);
   });
 });
